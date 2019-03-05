4101f4efc5d74826d8b7014e09e0b5846b4b481f
Sonawane <pooja.a.sonawane@capgemini.com>
2019-03-05 10:26:27 +0530

    File modified

--- Calculator.java
+++ Calculator.java
@@ -3,4 +3,7 @@ public class Calculator{
 			System.out.println("Hello World");
 			System.out.println("Bye");
 		}
+public static void addition(int number1,int number2){
+			System.out.println("Addition="+(number1 + number2));
+		}
 }
