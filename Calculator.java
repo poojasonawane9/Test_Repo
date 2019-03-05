4aedd694239edac72ca2ab8b1e5dac462696702a
Sonawane <pooja.a.sonawane@capgemini.com>
2019-03-05 10:42:20 +0530

    Added Substraction function

--- Calculator.java
+++ Calculator.java
@@ -6,4 +6,7 @@ public class Calculator{
 public static void addition(int number1,int number2){
 			System.out.println("Addition="+(number1 + number2));
 		}
+public static void substraction(int number1,int number2){
+			System.out.println("substraction="+(number1 - number2));
+		}
 }
